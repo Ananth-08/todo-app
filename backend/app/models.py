f0bd4a8dc367aa610aea8acae0874008f76895d9
Ananth-08 <ananthak1506@gmail.com>
2026-06-15 16:15:23 +0530

    Databse strucutre creatina d creation of tables

---
+++ backend/app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+
+from .database import Base
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    status = Column(String, default="pending")
+    created_at = Column(DateTime, default=datetime.utcnow)
